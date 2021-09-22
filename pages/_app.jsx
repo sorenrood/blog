7847714cc6400945c8ea131d28a020be1909d1ca
Justin Shaw <realjustinshaw@gmail.com>
2021-09-22 15:03:15 -0700

    fix: nextra patch

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,6 @@
 import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
